8ede7013a51c43af15d2fb88b00cb59c3cfdbbe7
christy welch <christinamawel@gmail.com>
2021-12-10 08:47:16 -0800

    create second test and recive expected fail

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -10,8 +10,22 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
+      //Arrange
+      //Act
       Bread testOrder = new Bread(5);
+      //Assert
       Assert.AreEqual(typeof(Bread), testOrder.GetType());
+    }
+    [TestMethod]
+    public void GetAmount_ReturnsAmount_Int()
+    {
+      //Arrange
+      int amount = 5;
+      //Act
+      Bread testOrder = new Bread(amount);
+      int result = testOrder.Amount;
+      //Assert
+      Assert.AreEqual(amount, result);
     }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,8 @@ namespace BreadOrder.Models
 {
   public class Bread
   {
+    public int Amount { get; }
+    
     public Bread(int amount)
     {
 
