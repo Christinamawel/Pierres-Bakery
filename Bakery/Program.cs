6b638294a682a92e227b4079c2ec421b1c141a7c
christy welch <christinamawel@gmail.com>
2021-12-10 10:02:17 -0800

    add start menu for UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using BreadOrder.Models;
+using PastryOrder.Models;
 
 namespace UI 
 {
@@ -6,7 +8,24 @@ namespace UI
   {
     static void Main()
     {
+      Console.WriteLine("---------------------------------------");
+      Console.WriteLine("Welcome To Pierre's Bakery!");
+      Console.WriteLine("---------------------------------------");
+      Console.WriteLine("Buy 2 loafs of bread get one free!");
+      Console.WriteLine("Get 3 pastries for $5!");
+      Console.WriteLine("---------------------------------------");
+      Console.WriteLine("Menu:");
+      Console.WriteLine("To Order Bread Type: order bread");
+      Console.WriteLine("To Order Pastries Type: order pastries");
+      Console.WriteLine("To View Order Total Type: view total");
       
+      string intput = Console.ReadLine();
+      int OrderTotal = 0;
+
+      if (input == "order bread")
+      {
+
+      }
     }
   }
 }
